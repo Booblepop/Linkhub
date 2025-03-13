249de0ede90edfa76d7b1ec1ec2a8c25ea6c42db
headzcode <kawasakipago@gmail.com>
2025-03-13 10:39:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Configuração do Firebase (substitua com suas credenciais)
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  databaseURL: "SEU_DATABASE_URL",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE_BUCKET",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+    apiKey: "AIzaSyDHkscCAj8pSyeN5KPXUn072_5XeSFF04M",
+  authDomain: "fire-news-f1be1.firebaseapp.com",
+  databaseURL: "https://fire-news-f1be1-default-rtdb.firebaseio.com",
+  projectId: "fire-news-f1be1",
+  storageBucket: "fire-news-f1be1.appspot.com",
+  messagingSenderId: "368411474818",
+  appId: "1:368411474818:web:567947238fa8f2da5fad9b"
 };
 
 // Inicialize o Firebase
